5920c3e9c49ba14cddb7468fe3e58369bb506bad
Emmy Denison <emmydenison@gmail.com>
2022-01-08 16:36:57 -0500

    wrote test for is_ghost_in_range

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -4,8 +4,16 @@ import java.io.*;
 
 public class TestGhostInRange extends TestCase {
 
-	public void testGhostInRange() throws FileNotFoundException{
-		return null;
+	public void testGhostInRange() throws FileNotFoundException {
+		NoFrame frame1 = new NoFrame();
+		Ghost ghost1 = frame1.addGhost(new Location(1, 1), "g1", Color.red);
+		PacMan pacman1 = frame1.addPacMan(new Location(1, 2));
+		assertTrue(pacman1.is_ghost_in_range());
+
+		NoFrame frame2 = new NoFrame();
+		Ghost ghost2 = frame2.addGhost(new Location(3, 6), "g2", Color.red);
+		Ghost ghost3 = frame2.addGhost(new Location(10, 9), "g3", Color.blue);
+		PacMan pacman2 = frame2.addPacMan(new Location(4, 8));
+		assertFalse(pacman2.is_ghost_in_range());
 	}
 }
-

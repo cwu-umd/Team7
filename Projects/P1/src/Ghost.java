028b6de7a48acacaeed10b9a900cc4e7ecb796bd
Emmy Denison <emmydenison@gmail.com>
2022-01-08 17:32:07 -0500

    implemented ghost attack

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -25,6 +25,10 @@ public class Ghost{
 	}
 
 	public boolean attack() {
-		return false;
+		if (this.is_pacman_in_range()) {
+			return myMap.attack(myName);
+		} else {
+			return false;
+		}
 	}
 }

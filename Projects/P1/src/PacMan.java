6f819f055e6a7dd900981567d395bde3276a282f
Emmy Denison <emmydenison@gmail.com>
2022-01-08 16:23:28 -0500

    implemented pacman is_ghost_in_range

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -2,11 +2,11 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import javax.swing.JComponent;
 
-public class PacMan{
+public class PacMan {
 	String myName;
 	Location myLoc;
 	Map myMap;
-	Location shift; 
+	Location shift;
 
 	public PacMan(String name, Location loc, Map map) {
 		this.myLoc = loc;
@@ -15,18 +15,31 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		return null;
 	}
 
 	public boolean move() {
 		return false;
 	}
 
-	public boolean is_ghost_in_range() { 
-		return false;
+	public boolean is_ghost_in_range() {
+		Location up = myLoc.shift(0, -1);
+		Location down = myLoc.shift(0, 1);
+		Location left = myLoc.shift(-1, 0);
+		Location right = myLoc.shift(1, 0);
+
+		if (myMap.getLoc(up).contains(Map.Type.GHOST) ||
+				myMap.getLoc(down).contains(Map.Type.GHOST) ||
+				myMap.getLoc(left).contains(Map.Type.GHOST) ||
+				myMap.getLoc(right).contains(Map.Type.GHOST) ||
+				myMap.getLoc(myLoc).contains(Map.Type.GHOST)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public JComponent consume() { 
- 		return null;
+	public JComponent consume() {
+		return null;
 	}
 }

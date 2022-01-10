17c809a21cbf1c09112ed119e52a9d86afa1651b
JosephinePalmhede <epalmhede@gmail.com>
2022-01-10 12:16:27 -0500

    added test

--- Projects/P1/tests/TestConsume.java
+++ Projects/P1/tests/TestConsume.java
@@ -5,6 +5,13 @@ import java.io.*;
 public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
-		return null;	
+		MainFrame frame = new MainFrame();
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+
+		// pacman eats the cookie at (1,1)
+		assertEquals(pacman.consume(), 1);
+
+		// pacman tries to eat the cookie at (1,1) again
+		assertEquals(pacman.consume(), null);
 	}
 }

3aeaff45fc7d2125dff9bb14b636c5239cc737fd
JosephinePalmhede <epalmhede@gmail.com>
2022-01-10 13:13:01 -0500

    fixed consume test

--- Projects/P1/tests/TestConsume.java
+++ Projects/P1/tests/TestConsume.java
@@ -6,12 +6,13 @@ public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
 		MainFrame frame = new MainFrame();
-		PacMan pacman = frame.addPacMan(new Location(1, 1));
+		PacMan pacman = frame.addPacMan(new Location(0, 0));
+		CookieComponent cookie = new CookieComponent(0, 0, 1);
 
-		// pacman eats the cookie at (1,1)
-		assertEquals(pacman.consume(), 1);
+		// pacman eats the cookie at (0,0)
+		assertEquals(pacman.consume(), cookie);
 
-		// pacman tries to eat the cookie at (1,1) again
+		// pacman tries to eat the cookie at (0,0) again
 		assertEquals(pacman.consume(), null);
 	}
 }

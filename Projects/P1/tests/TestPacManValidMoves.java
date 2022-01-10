0ea8d0186f83f5378e95d2c6d038297dd55db257
Shira_Shamouilian <sshamoui@terpmail.umd.edu>
2022-01-10 14:49:52 -0500

    valid_moves test

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,18 +1,16 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
 
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
 		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
-		PacMan pacman = frame.addPacMan(new Location(0,0));
-		//ArrayList<Location> l1 = new ArrayList<>();
-		// ans1.add(new Location(1,3));
-		// ans1.add(new Location(1,1));
-		System.out.println(pacman.get_valid_moves().get(0));
-		//assertEquals(ghost1.get_valid_moves(), ans1);
-
-		//return null;
+		PacMan pacman = frame.addPacMan(new Location(1,1));
+		ArrayList<Location> l1 = new ArrayList<>();
+		l1.add(new Location(1,2));
+		l1.add(new Location(2,1));
+		assertEquals(pacman.get_valid_moves(), l1);
 	}
 }

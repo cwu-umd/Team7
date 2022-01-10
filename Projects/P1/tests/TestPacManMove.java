11a8145a5da9fab6c52ea6b69c97f8d7cff7336a
unknown <Robert301301@gmail.com>
2022-01-09 20:16:17 -0500

    added test for pacman move

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -5,6 +5,12 @@ import java.io.*;
 public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
-		return null;
+		NoFrame frame1 = new NoFrame();
+		PacMan pacman1 = frame1.addPacMan(new Location(1, 2));
+		assertTrue(pacman1.move());
+
+		MainFame frame2 = new MainFrame();
+		PacMan pacman2 = frame2.addPacMan(new Location(1, 2));
+		assertTrue(pacman1.move());
 	}
 }

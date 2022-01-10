7e94da19adec9aa5f954699da9e0f0b4ed59be97
Emmy Denison <emmydenison@gmail.com>
2022-01-10 13:43:13 -0500

    fixed error in test

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -9,12 +9,12 @@ public class TestMapEatCookie extends TestCase {
 		MainFrame frame = new MainFrame();
 		PacMan pacman = frame.addPacMan(new Location(1, 1));
 
-		//assertTrue(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.COOKIE));
+		assertTrue(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.COOKIE));
 		assertEquals(frame.getMap().getCookies(), 0);
 
 		// pacman eats the cookie at (1,1)
 		frame.getMap().eatCookie("pacman");
-		//assertFalse(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.COOKIE));
+		assertFalse(frame.getMap().getLoc(new Location(1, 1)).contains(Map.Type.COOKIE));
 		assertEquals(frame.getMap().getCookies(), 1);
 
 		// pacman tries to eat the cookie at (1,1) again

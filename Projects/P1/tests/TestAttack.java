0814025e10e70ed9997f06ea8934e49a6cbf1f2f
Emmy Denison <emmydenison@gmail.com>
2022-01-10 13:18:35 -0500

    updated pacman and ghost locations

--- Projects/P1/tests/TestAttack.java
+++ Projects/P1/tests/TestAttack.java
@@ -12,9 +12,9 @@ public class TestAttack extends TestCase {
 
 		NoFrame frame2 = new NoFrame();
 		Ghost ghost2 = frame2.addGhost(new Location(3, 6), "g2", Color.red);
-		Ghost ghost3 = frame2.addGhost(new Location(10, 9), "g3", Color.blue);
-		Ghost ghost4 = frame2.addGhost(new Location(5, 8), "g4", Color.blue);
-		PacMan pacman2 = frame2.addPacMan(new Location(4, 8));
+		Ghost ghost3 = frame2.addGhost(new Location(10, 9), "g3", Color.red);
+		Ghost ghost4 = frame2.addGhost(new Location(4, 3), "g4", Color.red);
+		PacMan pacman2 = frame2.addPacMan(new Location(3, 3));
 		assertFalse(ghost2.attack());
 		assertFalse(ghost3.attack());
 		assertTrue(ghost4.attack());	

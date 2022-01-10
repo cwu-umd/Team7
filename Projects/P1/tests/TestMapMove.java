ce911219acc839e154472c65dd89899cf5ef1c8f
JosephinePalmhede <epalmhede@gmail.com>
2022-01-10 13:30:37 -0500

    map-move

--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -6,16 +6,16 @@ public class TestMapMove {
 
 	public void testMapMove() {
 		MainFrame frame = new MainFrame();
-		PacMan pacman = frame.addPacMan(new Location(0, 1));
-		Ghost ghost = frame.addGhost(new Location(0, 3), "ghost", Color.red);
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+		Ghost ghost = frame.addGhost(new Location(1, 3), "ghost", Color.red);
 
 		//try moving pacman
-		frame.getMap().move("pacman", new Location(0, 2), Map.Type.PACMAN);
+		frame.getMap().move("pacman", new Location(1, 2), Map.Type.PACMAN);
 		//use ghost in range to check if pacman moved
 		assertTrue(pacman.is_ghost_in_range());
 
 		//try moving ghost
-		frame.getMap().move("ghost", new Location(0, 4), Map.Type.GHOST);
+		frame.getMap().move("ghost", new Location(1, 4), Map.Type.GHOST);
 		assertFalse(pacman.is_ghost_in_range());
 
 

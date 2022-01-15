d57db3adb0993edf9c83d5c7497fdc220b867014
unknown <Robert301301@gmail.com>
2022-01-15 15:34:10 -0500

    fixed syntax

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -4,17 +4,15 @@ import java.io.*;
 
 public class TestGhostMove extends TestCase {
 
-	public void testGhostMove() throws FileNotFoundException{
+	public void testPacManMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 		//MainFrame frame = new MainFrame();
 
 		//Creating Players
 		Ghost ghost = frame.addGhost(new Location(0, 1), "name", Color.red); //Creates a red ghost named "name" at location x,y
 		Location l1 = ghost.get_valid_moves().get(0);
-		assertEqual(ghost.myLoc,new Location(0, 1) );
+		assertEquals(ghost.myLoc,new Location(0, 1) );
 		ghost.move();
-		assertEqual(ghost.myLoc,l1 );
-		
-	
+		assertEquals(ghost.myLoc,l1 );
 	}
 }

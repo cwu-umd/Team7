2af2440e7ac0ce8be45e4ad4e16e5e73894a4d33
Emmy Denison <emmydenison@gmail.com>
2022-01-16 21:34:50 -0500

    fixed error in PacMan.java

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -74,9 +74,9 @@ public class PacMan{
 		//This method checks to see if there is a 'power-cookie' located in Pacman's current coordinate. 
 		//If there is, this method calls the eatCookie method from the Map Class, and returns the cookie 
 		//component if the cookie a consumed, and null otherwise.
-		if (myMap.getLoc(myLoc).contains(Map.Type.COOKIE)){
+		if (!myMap.getLoc(myLoc).contains(Map.Type.COOKIE)){
 			return myMap.eatCookie("pacman");
 		}
- 		return 0;
+ 		return null;
 	}
 }
